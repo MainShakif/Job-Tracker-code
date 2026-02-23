1e34c13dcd74d9172e1898747148c1fb9aef70b9
MainShakif <shakifahmed27@gmail.com>
2026-02-23 15:53:51 +0600

    set interview button functionality applied.

--- script/script.js
+++ script/script.js
@@ -7,3 +7,31 @@ var rejectedTotal = 0;
 var totalJobs = 8;
 
 var cards = document.getElementsByClassName("job-card");
+
+var interviewButtons = document.getElementsByClassName("interview-btn");
+
+// interview button functionality
+for (var i = 0; i < interviewButtons.length; i++) {
+  interviewButtons[i].addEventListener("click", function () {
+    var card = this.closest(".job-card");
+    var badge = card.querySelector(".status-badge");
+    var oldStatus = badge.innerText;
+
+    if (oldStatus === "REJECTED") {
+      rejectedTotal--;
+    }
+
+    if (oldStatus !== "INTERVIEW") {
+      interviewTotal++;
+    }
+
+    badge.innerText = "INTERVIEW";
+
+    // badge color change
+    badge.classList.remove("badge-neutral", "badge-error");
+    badge.classList.add("badge-success");
+
+    interviewCount.innerText = interviewTotal;
+    rejectedCount.innerText = rejectedTotal;
+  });
+}
