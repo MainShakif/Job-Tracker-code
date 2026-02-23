74143ccbb9a1fdf3b1a3bc9475e48392d616ee11
MainShakif <shakifahmed27@gmail.com>
2026-02-23 15:47:26 +0600

    get dashboard elements , set initial values and  select cards.

---
+++ script/script.js
@@ -0,0 +1,9 @@
+var interviewCount = document.getElementById("interviewCount");
+var rejectedCount = document.getElementById("rejectedCount");
+var tabCount = document.getElementById("tabCount");
+
+var interviewTotal = 0;
+var rejectedTotal = 0;
+var totalJobs = 8;
+
+var cards = document.getElementsByClassName("job-card");
